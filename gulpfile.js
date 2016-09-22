d7ebc2eb8d6e3da1119cf125bf89185ca3b0b982
Harman Kamboj <playcivil@gmail.com>
2016-09-22 21:39:11 +0530

    Gulp Tasks for Styles added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
 var gulp = require('gulp');
+var notify = require('gulp-notify');
+var stylus = require('gulp-stylus');
+var koutoSwiss = require('kouto-swiss');
+var postcss = require('gulp-postcss');
+// POST CSS PLUGINS
+var autoprefixer = require('autoprefixer');
+var lost = require('lost');
+var rucksack = require('rucksack-css');
+var mqpack = require('css-mqpacker');
+var cssnano = require("cssnano");
 
 gulp.task("default", function() {
 	//Some Stuff here
+});
+
+gulp.task('styles', function() {
+
+	var processors = [
+		lost({}),
+		rucksack({}),
+		mqpack({
+			sort: true
+		}),
+		cssnano({}),
+		autoprefixer({browsers: ['last 1 version']}),
+	];
+
+    gulp.src('stylus/style.styl')
+      .pipe(stylus({
+      	'use': koutoSwiss()
+      }))
+      .pipe(postcss(processors))
+      .pipe(gulp.dest('css'))
+      .pipe(notify('Styles Compiled!'));
+});
+
+gulp.task('watch', function() {
+    // content
 });
